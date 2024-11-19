ec777cb20df637704bb55506e1a13ecb4d4aa837
didi <git@d10r.net>
2024-11-19 15:09:24 +0100

    initial import

---
+++ hardhat.config.js
@@ -0,0 +1,11 @@
+require("@nomiclabs/hardhat-ethers");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.27",
+  networks: {
+    hardhat: {
+      allowUnlimitedContractSize: true,
+    },
+  },
+};
